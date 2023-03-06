052252b2bb5d864f200b5a31a77e24f6976b124f
g0ren <g0ren@mail.ru>
2023-03-06 15:36:06 +0200

    Add project files.

---
+++ RegistryManagement/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace RegistryManagement
+{
+    class Program
+    {
+        public static void Main()
+        {
+            RegistryManager registryManager = new RegistryManager();
+            Console.WriteLine("Welcome to console registry manager!\n");
+            Console.WriteLine("Warning: some actions may cause harm to your" +
+                "system and require Administrator privileges");
+            while (true)
+            {
+                Console.WriteLine($"Selected key is {registryManager.SelectedKeyName}");
+                Console.WriteLine("Enter command");
+                Console.WriteLine("1 - Select a key");
+                Console.WriteLine("2 - List all values of selected key in format \"name: value\"");
+                Console.WriteLine("3 - Create or edit a value in selected key");
+                Console.WriteLine("4 - Create a new subkey in selected key");
+                Console.WriteLine("5 - Delete a subkey in selected key with all its children");
+                Console.WriteLine("0 - Exit");
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "0":
+                        registryManager.Exit();
+                        return;
+                    case "1":
+                        registryManager.NavigationLoop();
+                        break;
+                    case "2":
+                        registryManager.GetAllValues();
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter the name of the value you'd like to edit. " +
+                            "If the value does not exist it will be created");
+                        string valueName = Console.ReadLine();
+                        Console.WriteLine($"Enter the value of {valueName}");
+                        string valueValue = Console.ReadLine();
+                        registryManager.CreateOrEditValue(valueName,valueValue);
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter new key's name");
+                        registryManager.CreateSubKey(Console.ReadLine());
+                        break;
+                    case "5":
+                        Console.WriteLine("Enter the name of the subkey to delete");
+                        registryManager.DeleteSubKey(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ RegistryManagement/RegistryManager.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Win32;
+
+namespace RegistryManagement
+{
+    class RegistryManager
+    {
+        RegistryKey?[] keys = new RegistryKey?[]
+        {
+            null,
+            Registry.ClassesRoot,
+            Registry.CurrentUser,
+            Registry.LocalMachine,
+            Registry.Users
+        };
+
+        RegistryKey? selectedKey = null;
+        //StringBuilder selectedKeyNameBuilder = new StringBuilder();
+        public string SelectedKeyName
+        {
+            get 
+            { 
+                if(selectedKey == null)
+                {
+                    return "None";
+                }
+                else 
+                {
+                    return selectedKey.Name;
+                }
+            }
+        }
+
+        public void Exit()
+        {
+            if (selectedKey != null) 
+            { 
+                selectedKey.Close(); 
+            }    
+            selectedKey = null;
+        }
+
+        public RegistryKey? Navigation()
+        {
+            Console.WriteLine("Select key");
+            for(int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i] == null)
+                {
+                    Console.WriteLine($"{i} Exit");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} {keys[i].Name}");
+                }
+            }
+            int selection = Convert.ToInt32(Console.ReadLine());
+            return keys[selection];
+        }
+
+        public RegistryKey? Navigation(RegistryKey? rk)
+        {
+
+            List<string?> subkeys = new List<string?> { null };
+            foreach(string name in rk.GetSubKeyNames())
+            {
+                subkeys.Add(name);
+            }
+            Console.WriteLine("Select key");
+            for (int i = 0; i < subkeys.Count; i++)
+            {
+                if (subkeys[i] == null)
+                {
+                    Console.WriteLine($"{i} Exit");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} {subkeys[i]}");
+                }
+            }
+            int selection = Convert.ToInt32(Console.ReadLine());
+            var keyname = subkeys[selection];
+            RegistryKey key;
+            try
+            {
+                key = rk.OpenSubKey(keyname);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            return key;
+        }
+
+        public void NavigationLoop()
+        {
+            Exit();
+            RegistryKey? currentKey = Navigation();
+            if (currentKey != null)
+            {
+                selectedKey = currentKey;
+            }
+            while (true)
+            {
+                if(currentKey == null)
+                {
+                    return;
+                }
+                currentKey = Navigation(currentKey);
+                if (currentKey != null)
+                    selectedKey = currentKey;
+            }
+        }
+
+        public void GetAllValues()
+        {
+            if (selectedKey == null) 
+            { 
+                return; 
+            }
+                
+           foreach(var name in selectedKey.GetValueNames())
+            {
+                if (name == null)
+                {
+                    continue;
+                }
+                var value = selectedKey.GetValue(name);
+                Console.WriteLine($"{name}: {value}");
+            }
+        }
+
+        public void CreateOrEditValue(string valueName, string valueValue)
+        {
+            selectedKey.SetValue(valueName, valueValue);
+        }
+
+        public void CreateSubKey(string name)
+        {
+            selectedKey.CreateSubKey(name);
+        }
+
+        public void DeleteSubKey(string name)
+        {
+            selectedKey.DeleteSubKeyTree(name);
+        }
+    }
+}
